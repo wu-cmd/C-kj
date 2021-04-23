64d5ecaa8d8230510b4ac25fcc6a2937b93a19fc
吴海龙 <whl@qq.com>
2021-04-23 17:04:07 +0800

    练习商城

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
 <!-- 最近更新2020-10-26 添加完毕购物车功能！！！ -->
   <div id="app">
-    <keep-alive exclude="Detail,CategoryData">
-      <!-- keep-alive 缓存    exclude="Detail" 这页面不缓存 -->
+    <!-- <keep-alive exclude="Detail,CategoryData"> -->
+
     <router-view></router-view>
-    </keep-alive>
+    <!-- </keep-alive> -->
     <main-tab-bar></main-tab-bar>
   </div>
 </template>
